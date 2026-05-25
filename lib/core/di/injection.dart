d5b85a0f638353f185a0930a6e3ede9bf6416b67
Far14z <faridce14@gmail.com>
2026-05-25 13:14:42 -0500

    feat(config): add bloc enums states and catalog dependency injection.

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -5,13 +5,22 @@ import 'package:intiva_mobile_application/core/network/interceptor/auth_intercep
 import 'package:intiva_mobile_application/core/navigation/router/app_router.dart';
 import 'package:intiva_mobile_application/core/navigation/router/auth_guard.dart';
 import 'package:intiva_mobile_application/core/storage/token_storage.dart';
+import 'package:intiva_mobile_application/features/catalog/domain/repositories/vehicle_repository.dart';
 import 'package:intiva_mobile_application/features/iam/login/domain/repositories/auth_repository.dart';
 import 'package:intiva_mobile_application/features/iam/login/data/repositories/auth_repository_impl.dart';
 import 'package:intiva_mobile_application/features/iam/login/data/services/remote/auth_service.dart';
 import 'package:intiva_mobile_application/features/iam/login/presentation/blocs/login_bloc.dart';
+import 'package:intiva_mobile_application/features/catalog/data/remote/services/vehicle_service.dart';
+import 'package:intiva_mobile_application/features/catalog/data/repositories/vehicle_repository_impl.dart';
+import 'package:intiva_mobile_application/core/network/api/api_endpoints.dart';
+import 'package:intiva_mobile_application/features/catalog/presentation/bloc/catalog_bloc.dart';
 
 final getIt = GetIt.instance;
 
+/// Configures all dependencies for the application using GetIt.
+/// This includes services, repositories, and Blocs for various features.
+/// 
+/// The function is asynchronous to allow for any necessary initialization of dependencies.
 Future<void> configureDependencies() async {
   // Storage
   const secureStorage = FlutterSecureStorage(
@@ -25,10 +34,6 @@ Future<void> configureDependencies() async {
     AuthInterceptor(getIt<FlutterSecureStorage>()),
   );
   getIt.registerSingleton<DioClient>(DioClient(getIt<AuthInterceptor>()));
-
-  // Router
-  getIt.registerSingleton<AuthGuard>(AuthGuard(getIt<FlutterSecureStorage>()));
-  getIt.registerSingleton<AppRouter>(AppRouter(getIt<AuthGuard>()));
 
   // Storage helper
   getIt.registerLazySingleton<TokenStorage>(
@@ -46,4 +51,21 @@ Future<void> configureDependencies() async {
   getIt.registerFactory<LoginBloc>(
     () => LoginBloc(repository: getIt<AuthRepository>()),
   );
+
+  // Catalog
+  getIt.registerLazySingleton<VehicleService>(
+    () => VehicleService(getIt<DioClient>().dio, baseUrl: ApiEndpoints.baseUrl),
+  );
+
+  getIt.registerLazySingleton<VehicleRepository>(
+    () => VehicleRepositoryImpl(getIt<VehicleService>()),
+  );
+
+  getIt.registerFactory<CatalogBloc>(
+    () => CatalogBloc(getIt<VehicleRepository>()),
+  );
+
+  // Router
+  getIt.registerSingleton<AuthGuard>(AuthGuard(getIt<FlutterSecureStorage>()));
+  getIt.registerSingleton<AppRouter>(AppRouter(getIt<AuthGuard>()));
 }
--- lib/core/enums/status.dart
+++ lib/core/enums/status.dart
@@ -1,1 +1,2 @@
+/// An enum representing the status of an operation or process.
 enum Status { initial, loading, success, failure }
